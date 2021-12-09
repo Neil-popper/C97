15111c96a66b3ed2965101698d7920d0ba2f22dd
Neil-popper <77514497+Neil-popper@users.noreply.github.com>
2021-12-09 15:36:13 -0600

    Add files via upload

---
+++ basic97.py
@@ -0,0 +1,18 @@
+yourName="Neil"
+print (yourName)
+hobbies=input("WHAT ARE YOUR HOBBIES?")
+print(hobbies)
+grade=int(input("What is your grade?"))
+print(grade)
+if (grade==5):
+    subject=input("What is your favorite subject")
+    print(subject)
+elif (grade==6):
+    icecream=input("What is your favorite ice cream flavour?")
+    print(icecream)
+else:
+    print("Ok")
+freindlist=["jason","mark","thomas"]
+print(freindlist[1])
+for i in range(0,10):
+    print("hi")
